5d4f2c5d3965dddb9d73144adbdd318834900107
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-05-20 20:31:03 +0100

    10. Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,16 +8,16 @@
  */
 size_t binary_tree_depth(const binary_tree_t *node)
 {
-    size_t depth = 0;
+	size_t depth = 0;
 
-    if (node == NULL)
-        return 0;
+	if (node == NULL)
+		return 0;
 
-    while (node->parent != NULL)
-    {
-        depth++;
-        node = node->parent;
-    }
+	while (node->parent != NULL)
+	{
+		depth++;
+		node = node->parent;
+	}
 
-    return depth;
+	return depth;
 }
